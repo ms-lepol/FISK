2029d634200f0381531f65dfaac02d1431317d77
ms-lepol <maillardsalinpaul@gmail.com>
2023-12-05 12:44:29 +0100

    Player.cc - Implementation of setters & getters - TODO DrawCard & PlayCard

--- src/common/Land.h
+++ src/common/Land.h
@@ -2,8 +2,23 @@
 #include <vector>
 
 class Land {
-    unsigned nb_units;
-    std::string name;
-    std::vector<unsigned> neighbors;
-    unsigned player_id;
+    private:
+        unsigned land_id;
+        unsigned nb_units;
+        std::string name;
+        std::vector<unsigned> neighbors;
+        unsigned owner_id;
+    public:
+        Land(unsigned nb_units, std::string name, std::vector<unsigned> neighbors, unsigned player_id);
+        unsigned getNb_units();
+        std::string getName();
+        std::vector<unsigned> getNeighbors();
+        unsigned getPlayer_id();
+        void setOwner_id(unsigned player_id);
+        void addNeighbor(unsigned neighbor);
+        void removeNeighbor(unsigned neighbor);
+        //Actions of the player
+        void reinforce(unsigned nb_units);
+        void attack(unsigned land_id);
+        void fortify(unsigned land_id);
 };
--- src/common/Player.h
+++ src/common/Player.h
@@ -1,13 +1,15 @@
 #include <gf/Color.h>
+#include <vector>
 namespace fisk {
     class Player {
         private:
             unsigned id_client;
             unsigned nb_units;
             gf::Color4f color;
-            unsigned hand[];
+            std::vector<unsigned> hand;
         public:
-            Player(int id_client, int nb_units, gf::Color4f color, int hand[]);
+            Player(int id_client, int nb_units, gf::Color4f color, std::vector<unsigned> hand);
+            Player(int id_client, int nb_units, gf::Color4f color, unsigned card...);
             int getId_client();
             int getNb_units();
             gf::Color4f getColor();
