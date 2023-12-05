4585d6b69d686df8d023454ad53929e850b5db50
mimil <emilien.decoux@free.fr>
2023-12-05 11:34:51 +0100

    tout dans fisk

--- src/common/Game.h
+++ src/common/Game.h
@@ -7,33 +7,37 @@
 #include "Card.h"
 #include "Continent.h"
 
-class Game {
-    std::vector<fisk::Player> players;
-    std::vector<Land> lands;
-    std::vector<Card> cards;
-    std::vector<Continent> continents;
+namespace fisk {
 
-    int game_state;
+    class Game {
+        std::vector<fisk::Player> players;
+        std::vector<Land> lands;
+        std::vector<Card> cards;
+        std::vector<Continent> continents;
 
-    public:
+        int game_state;
 
-    /**
-     * add a new player and return the ID
-     */
-    fisk::PlayerId add_player(Player player);
+        public:
 
-    /**
-     * add a new land and return the ID
-     */
-    fisk::LandId add_land(Land land);
+        /**
+         * add a new player and return the ID
+         */
+        fisk::PlayerId add_player(Player player);
 
-    /**
-     * add a new card and return the ID
-     */
-    fisk::CardId add_card(Card card);
+        /**
+         * add a new land and return the ID
+         */
+        fisk::LandId add_land(Land land);
 
-    /**
-     * add a new continent and return the ID
-     */
-    fisk::ContinentId add_continent(Continent continent);
-};
+        /**
+         * add a new card and return the ID
+         */
+        fisk::CardId add_card(Card card);
+
+        /**
+         * add a new continent and return the ID
+         */
+        fisk::ContinentId add_continent(Continent continent);
+    };
+
+}
--- src/common/Land.h
+++ src/common/Land.h
@@ -1,9 +1,12 @@
 #include <string>
 #include <vector>
 
-class Land {
-    unsigned nb_units;
-    std::string name;
-    std::vector<unsigned> neighbors;
-    unsigned player_id;
-};
+namespace fisk {
+
+    class Land {
+        unsigned nb_units;
+        std::string name;
+        std::vector<unsigned> neighbors;
+        unsigned player_id;
+    };
+}
