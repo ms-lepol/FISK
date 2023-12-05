011f44c74682dbe2adb9b053bc12f31809d480ef
mimil <emilien.decoux@free.fr>
2023-12-05 11:31:22 +0100

    better types for IDs in Game.h

--- src/common/Game.h
+++ src/common/Game.h
@@ -1,12 +1,14 @@
 #include <vector>
 
+#include "Constants.h"
+
 #include "Player.h"
 #include "Land.h"
 #include "Card.h"
 #include "Continent.h"
 
 class Game {
-    std::vector<Player> players;
+    std::vector<fisk::Player> players;
     std::vector<Land> lands;
     std::vector<Card> cards;
     std::vector<Continent> continents;
@@ -18,20 +20,20 @@ class Game {
     /**
      * add a new player and return the ID
      */
-    unsigned add_player(Player player);
+    fisk::PlayerId add_player(Player player);
 
     /**
      * add a new land and return the ID
      */
-    unsigned add_land(Land land);
+    fisk::LandId add_land(Land land);
 
     /**
      * add a new card and return the ID
      */
-    unsigned add_card(Card card);
+    fisk::CardId add_card(Card card);
 
     /**
      * add a new continent and return the ID
      */
-    unsigned add_continent(Continent continent);
+    fisk::ContinentId add_continent(Continent continent);
 };
