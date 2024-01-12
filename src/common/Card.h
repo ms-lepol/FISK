393c8ca9eb8ff0435670e4ae1ea17c71ad00d9c8
mimil <emilien.decoux@protonmail.com>
2024-01-12 12:01:44 +0100

    add serialisation exemple on Card

--- src/common/Card.h
+++ src/common/Card.h
@@ -1,8 +1,23 @@
+#include "Constants.h"
+#ifndef FISK_CARD_H
+#define FISK_CARD_H value
+
+#include "gf/Serialization.h"
 #include "Enums.h"
 
 namespace fisk {
     class Card {
         fisk::Type type_unit;
         unsigned land_id;
+
+        public:
+
+        fisk::Type getType() const;
+        LandId getLand() const;
     };
+
+    template<typename S> // Serializer or Deserializer
+    S& operator|(S& sa, Card const& card);
 }
+
+#endif /* ifndef FISK_CARD_H */
