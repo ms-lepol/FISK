061b78fc5eaffbf82f836acd49bae859295d367b
mimil <emilien.decoux@protonmail.com>
2024-01-25 12:31:15 +0100

    on mange et après on taf

--- src/client/GameHub.cc
+++ src/client/GameHub.cc
@@ -1,18 +1,19 @@
 #include "GameHub.h"
 
 
-namespace fisk { 
+namespace fisk {
     GameHub::GameHub() : GameManager("FISK", { "../data/" })
-    , mainScene(*this) 
-    , cardScene(*this) 
-    , lobbyScene(*this) 
+    , mainScene(*this)
+    , cardScene(*this)
+    , lobbyScene(*this)
     , titleScene(*this) {
 
-        
+
+        clientNetwork.connect(HOSTNAME);
 
         mainScene.setWorldViewSize({ 1280, 720 });
         pushScene(mainScene);
         mainScene.pause();
         pushScene(lobbyScene);
     }
-} 
+}
